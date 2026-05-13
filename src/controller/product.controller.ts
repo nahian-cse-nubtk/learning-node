4f440f9bfdc0c185cd4f481052b8edccc5f71220
nahian-cse-nubtk <sheikhalnahian@gmail.com>
2026-05-13 23:38:17 +0600

    post, put and delete method is implement in the node server

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,33 +1,105 @@
 import type { IncomingMessage, ServerResponse } from "http";
-import { readProduct } from "../service/product.service";
-import type { IProduct } from './../types/product.type';
-
-export const productController =(req:IncomingMessage, res:ServerResponse)=>{
-        const url = req.url;
-        const method = req.method;
-        const urlParts = url?.split("/");
-        const id = urlParts && urlParts[1] ==="products" ? Number(urlParts[2]) : null
+import { insertProduct, readProduct } from "../service/product.service";
+import type { IProduct } from "./../types/product.type";
+import { parseBody } from "../utilities/parseBody";
 
 
+export const productController = async (
+  req: IncomingMessage,
+  res: ServerResponse,
+) => {
+  const url = req.url;
+  const method = req.method;
+  const urlParts = url?.split("/");
+  const id =
+    urlParts && urlParts[1] === "products" ? Number(urlParts[2]) : null;
 
+  if (url === "/products" && method === "GET") {
+    const products = readProduct();
 
-        if(url==='/products' && method ==="GET"){
+    res.writeHead(200, { "content-type": "application/json" });
 
-            const products = readProduct();
+    res.end(
+      JSON.stringify({ message: "The is product route", data: products }),
+    );
+  } else if (method === "GET" && id !== null) {
+    const products = readProduct();
 
+    const product = products.find((p: IProduct) => p.id === id);
 
-            res.writeHead(200,{"content-type":"application/json"})
+    res.writeHead(200, { "content-type": "application/json" });
 
-            res.end(JSON.stringify({message:"The is product route",data: products}))
-        }
-        else if(method === 'GET' && id!== null){
-            const products = readProduct();
+    res.end(
+      JSON.stringify({ message: "The is single product route", data: product }),
+    );
+  } else if (method === "POST" && url === "/products") {
+    const body = await parseBody(req);
+    const products = readProduct();
+    const newProduct = {
+      id: Date.now(),
+      ...body,
+    };
+    products.push(newProduct);
+    insertProduct(products);
+    res.writeHead(200, { "content-type": "application/json" });
 
-            const product = products.find((p:IProduct)=>p.id===id);
+    res.end(
+      JSON.stringify({
+        message: "Product is successfully inserted",
+        data: products,
+      }),
+    );
+  } else if (method === "PUT" && id !== null) {
+    const body = await parseBody(req);
+    const products = readProduct();
+    const index = products.findIndex((p: IProduct) => p.id === id);
 
-            res.writeHead(200,{"content-type":"application/json"})
+    if (index < 0) {
+      res.writeHead(404, { "content-type": "application/json" });
 
-            res.end(JSON.stringify({message:"The is product route",data: product}))
+      res.end(
+        JSON.stringify({
+          message: "Product not found",
+          data: null,
+        }),
+      );
+    }
+    products[index] = {
+        id: products[index].id,
+        ...body
+    }
+    insertProduct(products)
+    res.writeHead(200, { "content-type": "application/json" });
 
-        }
-}
+    res.end(
+      JSON.stringify({
+        message: "Product data is updated",
+        data: products,
+      }),
+    );
+  }
+  else if(method === "DELETE" && id!==null){
+    const products = readProduct()
+    const index = products.findIndex((p:IProduct)=>p.id===id)
+    if(index<0){
+        res.writeHead(404, { "content-type": "application/json" });
+
+      res.end(
+        JSON.stringify({
+          message: "Product not found",
+          data: null,
+        }),
+      );
+    }
+    products.splice(index,1);
+    insertProduct(products);
+    res.writeHead(200, { "content-type": "application/json" });
+
+    res.end(
+      JSON.stringify({
+        message: "Product id deleted",
+        data: products,
+      }),
+    );
+  }
+};
--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -7,7 +7,8 @@ export const readProduct =()=>{
 
     const products = fs.readFileSync(filePath,"utf-8")
     return JSON.parse(products);
+}
 
-
-
+export const insertProduct =(payload: any)=>{
+    fs.writeFileSync(filePath,JSON.stringify(payload))
 }
